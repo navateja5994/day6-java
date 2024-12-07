ef6027bd5e9c758bc3f5205d290462b071fbd3e6
NAVATEJA <navatejar80@gmail.com>
2024-12-07 11:39:55 +0530

    Add files via upload

---
+++ controlst.java
@@ -0,0 +1,20 @@
+class Main{
+    public static void main (String args[])
+    {
+        for( int i=0;i<5;i++)
+        {
+            if(i==2)
+            break;
+            System.out.println(i);
+        }
+        System.out.println("continue example");
+        for (int i=0;i<5;i++)
+        {
+            if(i==2)
+            continue;
+            System.out.println(i);
+        }
+
+
+    }
+}
