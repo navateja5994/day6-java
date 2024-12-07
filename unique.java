67cb80594f8a355934566f7bae227bec6c5f5d50
NAVATEJA <navatejar80@gmail.com>
2024-12-07 15:38:16 +0530

    Add files via upload

---
+++ unique.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class array
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int ans=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            {
+                ans^=arr[i];
+            }
+            System.o
+    }
+}
